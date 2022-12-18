3a4f5d53ef9eda08f7b009549fb93b220c7ecfe0
xkaze09 <47445339+xkaze09@users.noreply.github.com>
2022-12-19 00:48:19 +0800

    Add student capabilities

--- Interfaces/StudentInterface.java
+++ Interfaces/StudentInterface.java
@@ -1,11 +1,17 @@
 package Interfaces;
 
+import Helpers.ListHelper;
 import Models.Student;
+import Models.Feedback;
+import Models.Assignment;
+
+import java.util.List;
 
 public class StudentInterface {
     public void showMyDashboard() {
         System.out.println("""
                 \nHello, student. What do you want to do?
+
                 1. View existing assignments
                 2. View feedbacks
                 3. Mark an assignment as done
@@ -14,16 +20,24 @@ public class StudentInterface {
                 """);
     }
 
-    public void viewMyAssignments() {
+    public void viewMyAssignments(List<Assignment> studentAssignments) {
         // See assignments
+        if (!ListHelper.hasAssignments(studentAssignments))
+            return;
 
+        studentAssignments.forEach(task -> System.out.println(task.getAssignment(true)));
     }
 
-    public void viewMyFeedbacks() {
+    public void viewMyFeedbacks(List<Feedback> studentFeedbacks) {
         // See feedbacks
+        if (!ListHelper.hasFeedbacks(studentFeedbacks))
+            return;
+
+        studentFeedbacks.forEach(feedback -> System.out.println(feedback.getFeedback(true)));
     }
 
     public void viewSelfInfo(Student student) {
         // See self account info
+        student.viewSelfInfo();
     }
 }
