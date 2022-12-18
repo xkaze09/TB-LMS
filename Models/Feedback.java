ad82252b78bb8e2c83530bf4b0e4dab21d5c0edf
xkaze09 <47445339+xkaze09@users.noreply.github.com>
2022-12-18 22:24:10 +0800

    Add Feedback class

---
+++ Models/Feedback.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class Feedback {
+    private final String feedback;
+    private final String studentName;
+    private final String teacherName;
+
+    public Feedback(String studentName, String teacherName, String feedback) {
+        this.studentName = studentName;
+        this.teacherName = teacherName;
+        this.feedback = feedback;
+    }
+
+    public String getFeedback(boolean isStudent) {
+        return (isStudent) ? String.format("""
+                \nFrom Instructor %s: {
+                %s
+                }
+                """, teacherName, feedback) : String.format("""
+                \nTo Instructor %s: {
+                %s
+                }
+                """, studentName, feedback);
+
+    }
+
+    @Override
+    public String toString() {
+        return studentName + "," + teacherName + "," + feedback;
+    }
+}
